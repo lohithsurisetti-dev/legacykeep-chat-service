c41de50d04df4c4221c01c5bd6abaebd8cfa57ef
lohith <lohith@psense.ai>
2025-08-21 00:32:13 -0500

    Initial commit: chat-service microservice

---
+++ src/main/java/com/legacykeep/service/UchatUserviceApplication.java
@@ -0,0 +1,12 @@
+package com.legacykeep.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchatUserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UchatUserviceApplication.class, args);
+    }
+}
